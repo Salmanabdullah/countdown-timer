5ce72e5bf56923518d9f896f44827862104788b2
Salmanabdullah <salmandcc@gmail.com>
2023-11-26 20:34:16 +0100

    repeat and notification sound works

--- src/Components/inputArea.js
+++ src/Components/inputArea.js
@@ -1,41 +1,76 @@
 import React, { useState } from "react";
 import "./inputArea.css";
 import DisplayTimer from "./displayTimer";
+import soundAlert from "../assets/alert.wav";
 
 const Header = () => {
   const [timerExpired, setTimerExpired] = useState(false);
   const [timerKey, setTimerKey] = useState(0);
-  const [inputValue, setInputValue] = useState("");
+  const [inputMinutes, setInputMinutes] = useState("");
+  const [inputTimes, setInputTimes] = useState("");
 
+  /************* When times expires **************/
   const handleTimerExpire = () => {
     setTimerExpired(true);
-    // You can perform additional actions when the timer expires
+    let sound = new Audio(soundAlert);
+    sound.play().catch((error) => {
+      console.error("Error playing audio:", error);
+    });
+    if (inputTimes > 0) {
+      setInputTimes(inputTimes - 1);
+      handleStartTimer();
+    }
+    if (inputTimes === 0) {
+      setTimerExpired(true);
+    }
   };
+  /************* Countdown starts **************/
   const handleStartTimer = () => {
+    if (
+      isNaN(inputMinutes) ||
+      isNaN(inputTimes) ||
+      inputMinutes <= 0 ||
+      inputTimes <= 0
+    ) {
+      alert("Please enter valid values for minutes and repeat.");
+      return;
+    }
     setTimerExpired(false); // Reset the timer when starting
     setTimerKey(timerKey + 1);
   };
-
-  const handleChange = (e) => {
+  /************* input values **************/
+  const handleMinutesChange = (e) => {
     // Update the state with the current value of the input field
-    setInputValue(e.target.value);
+    setInputMinutes(e.target.value);
+  };
+
+  const handleTimesChange = (e) => {
+    // Update the state with the current value of the input field
+    setInputTimes(e.target.value);
   };
 
   return (
     <div>
       <section>
-        How many minutes:
+        Minutes:{" "}
         <input
           type="number"
           id="minutes"
           name="minutes"
-          value={inputValue}
-          onChange={handleChange}
+          value={inputMinutes}
+          onChange={handleMinutesChange}
           required
         />
       </section>
       <section>
-        How many times: <input type="number" required name="times" />
+        Repeat:{" "}
+        <input
+          type="number"
+          value={inputTimes}
+          onChange={handleTimesChange}
+          required
+          name="times"
+        />
       </section>
       <div id="start">
         <button id="startButton" onClick={handleStartTimer}>
@@ -44,13 +79,15 @@ const Header = () => {
       </div>
       <div>
         {!timerExpired ? (
-          <DisplayTimer
-            key={timerKey}
-            initialTimeInSeconds={inputValue * 60}
-            onTimerExpire={handleTimerExpire}
-          />
+          <div>
+            <DisplayTimer
+              key={timerKey}
+              initialTimeInSeconds={inputMinutes}
+              onTimerExpire={handleTimerExpire}
+            />
+          </div>
         ) : (
-          <p>Timer Expired!</p>
+          <p></p>
         )}
       </div>
     </div>
